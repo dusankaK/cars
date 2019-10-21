36171c4aec44d6df4bf0f3c3aa48645d52efc747
Dusanka Karac <karacdusanka11@gmail.com>
2019-10-21 18:55:45 +0200

    Zadatak 2

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Welcome to Cars Project
+                    About us
                 </div>
 
                 <div class="links">
