0b8398cb16e32af813880372e8861fed228ab97e
Dusanka Karac <karacdusanka11@gmail.com>
2019-10-21 19:03:20 +0200

    Zadatak 3

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,9 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $name = 'Dusanka';
+    $age = 26;
+    return view('welcome', compact('name', 'age'));
 });
 
 Route::get('/about', function () {
